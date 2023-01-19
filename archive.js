7105f2596ccaefae0b9be18b8ffa17321795ea96
Mahade Hassan <mahade.hassan.bd@gmail.com>
2023-01-19 18:34:49 +0600

    js correction

--- archive.js
+++ archive.js
@@ -5,8 +5,8 @@ const loadBooks = () => {
 }
 loadBooks();
 
-const displayBook = book => {
-    displaySingleBooks(book);
+const displayBook = bookk => {
+    console.log(bookk);
 }
 
 const searchBook = () => {
@@ -16,6 +16,7 @@ const searchBook = () => {
     document.getElementById('search-field').value = '';
 
 }
+
 const bookSearchText = searchText => {
     const url = `https://openlibrary.org/search.json?q=${searchText}`
     fetch (url)
@@ -26,20 +27,31 @@ const bookSearchText = searchText => {
 const displaySingleBooks = books => {
     console.log(books);
     const bookContainer = document.getElementById('book-archive');
-    bookContainer.textContent = '';
+    // bookContainer.textContent = '';
+    //  if (!books) {
+    //     const div = document.createElement('div');
+    //     div.innerHTML = `
+    //     <h2 class="fw-bold text-primary">Sorry! Book Not Found</h2>
+    //     `
+    //     bookContainer.appendChild(div); 
+    // }
 
     books.forEach(book => {
-         console.log(book);
+        console.log(book);
         const div = document.createElement('div');
         div.innerHTML = `
         <p><span class="fw-bold text-primary">Book Name:</span> <span class="fw-semibold">${book.title}</span></p>
 
         <p><span class="fw-bold text-primary">Author Name:</span> <span class="fw-semibold">${book.author_name}</span></p>
 
-        <p><span class="fw-bold text-primary">First Publish Date:</span> <span class="fw-semibold">${book.first_publish_year}</span></p>
+        <p><span class="fw-bold text-primary">First Publish Year:</span> <span class="fw-semibold">${book.first_publish_year}</span></p>
         `
-        bookContainer.appendChild(div);  
-    });
+        bookContainer.appendChild(div)
+    })
+
+   
+     
 }
 
-bookSearchText();
+bookSearchText('book');
+
