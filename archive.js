9bc3af496daa4312a67dd3d369ec696cf616ae9f
Mahade Hassan <mahade.hassan.bd@gmail.com>
2023-01-23 16:14:22 +0600

    js correction

--- archive.js
+++ archive.js
@@ -1,33 +1,32 @@
-const loadBooks = () => {
-    fetch(' https://openlibrary.org/search.json?q=javascript')
-        .then(res => res.json())
-    .then(data=>displayBook(data.docs))
-}
-loadBooks();
+// const loadBooks = () => {
+//     fetch(' https://openlibrary.org/search.json?q=javascript')
+//         .then(res => res.json())
+//     .then(data=>displaySingleBooks(data.docs[0]))
+// }
+// loadBooks();
 
-const displayBook = bookk => {
-    console.log(bookk);
-}
-
+// const displayBook = bookk => {
+//     console.log(bookk);
+// }
+// displayBook();
 const searchBook = () => {
-    const searchText = document.getElementById('search-field').value;
-    bookSearchText(searchText);
-
+    const serchText = document.getElementById('search-field').value;
+    bookSearchText(serchText);
     document.getElementById('search-field').value = '';
 
 }
 
 const bookSearchText = searchText => {
-    const url = `https://openlibrary.org/search.json?q=${searchText}`
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch (url)
     .then(res => res.json())
-    .then(data=>displaySingleBooks(data.books))
+    .then(data=>displaySingleBooks(data.docs))
 }
 
 const displaySingleBooks = books => {
-    console.log(books);
+    // console.log(books);
     const bookContainer = document.getElementById('book-archive');
-    // bookContainer.textContent = '';
+    bookContainer.textContent = '';
     //  if (!books) {
     //     const div = document.createElement('div');
     //     div.innerHTML = `
@@ -35,23 +34,21 @@ const displaySingleBooks = books => {
     //     `
     //     bookContainer.appendChild(div); 
     // }
-
-    books.forEach(book => {
+    // const book = undefined;
+    books.forEach(book=> {
         console.log(book);
+        // let { title, author_name, first_publish_year } = book;
         const div = document.createElement('div');
         div.innerHTML = `
-        <p><span class="fw-bold text-primary">Book Name:</span> <span class="fw-semibold">${book.title}</span></p>
+        <p><span class="fw-bold text-primary">Book Name:</span> <span class="fw-semibold">${book.title?book.title:'None'}</span></p>
 
-        <p><span class="fw-bold text-primary">Author Name:</span> <span class="fw-semibold">${book.author_name}</span></p>
+        <p><span class="fw-bold text-primary">Author Name:</span> <span class="fw-semibold">${book.author_name?book.author_name:'None'}</span></p>
 
-        <p><span class="fw-bold text-primary">First Publish Year:</span> <span class="fw-semibold">${book.first_publish_year}</span></p>
+        <p><span class="fw-bold text-primary">First Publish Year:</span> <span class="fw-semibold">${book.first_publish_year?book.first_publish_year:'None'}</span></p>
         `
         bookContainer.appendChild(div)
-    })
-
-   
-     
+    })  
 }
 
-bookSearchText('book');
+bookSearchText('cse');
 
