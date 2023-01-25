ca52fe61e63ef8cfa4736aa45b56c3ac441ba1e4
Mahade Hassan <mahade.hassan.bd@gmail.com>
2023-01-25 11:16:50 +0600

    add class

--- archive.js
+++ archive.js
@@ -1,14 +1,4 @@
-// const loadBooks = () => {
-//     fetch(' https://openlibrary.org/search.json?q=javascript')
-//         .then(res => res.json())
-//     .then(data=>displaySingleBooks(data.docs[0]))
-// }
-// loadBooks();
 
-// const displayBook = bookk => {
-//     console.log(bookk);
-// }
-// displayBook();
 const searchBook = () => {
     const serchText = document.getElementById('search-field').value;
     bookSearchText(serchText);
@@ -25,21 +15,22 @@ const bookSearchText = searchText => {
 
 const displaySingleBooks = books => {
     // console.log(books);
-    let cover_i;
+    // let cover_i;
     const bookContainer = document.getElementById('book-archive');
     bookContainer.textContent = '';
-    //  if (!books) {
-    //     const div = document.createElement('div');
-    //     div.innerHTML = `
-    //     <h2 class="fw-bold text-primary">Sorry! Book Not Found</h2>
-    //     `
-    //     bookContainer.appendChild(div); 
-    // }
+     if (!books) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <h2 class="fw-bold text-primary">Sorry! Book Not Found</h2>
+        `
+        bookContainer.appendChild(div); 
+    }
     
     books.forEach(book=> {
         console.log(book);
         const imgURL = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
         const div = document.createElement('div');
+        div.classList.add('cards') ;
         div.innerHTML = `
     <div class="card h-100 " style="width: 18rem;">
         <img src="${imgURL}" class="card-img-top" alt="...">
@@ -48,8 +39,7 @@ const displaySingleBooks = books => {
 
              <p><span class="fw-bold text-primary">Author Name:</span> <span class="fw-semibold">${book.author_name?book.author_name:'None'}</span></p>
 
-            <p><span class="fw-bold text-primary">First Publish Year:</span> <span class="fw-semibold">${book.first_publish_year?book.first_publish_year:'None'}</span></p> 
-
+            <p><span class="fw-bold text-primary">First Publish Year:</span> <span class="fw-semibold">${book.first_publish_year?book.first_publish_year:'None'}</span></p>
         </div>
     </div>       
         `
